b2dfe1cb123543ac97326a396b017ed50d963d79
Roman Micuda <micuda.roman@gmail.com>
2022-02-16 23:24:14 +0100

    hello world

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -1,4 +1,14 @@
 package sk.stuba.fei.uim.oop;
 
 public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello worrld");
+        int number1 =1;
+        long number2 = 5L;
+        float number3 = 5.5f;
+        double number4 = 5.5;
+        char letter = 'a';
+        String string = "aaaaa";
+        boolean bool = true;
+    }
 }
